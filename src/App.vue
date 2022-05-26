a8460e8d1b08b24fd32477094e365ec7be47bc02
Gabriel Amadé <gabrielamade@icloud.com>
2022-05-26 23:49:24 +0200

    première version

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,114 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <header>
+      <h1>The <strong>Anime</strong>Database</h1>
+      <form class="search-box">
+        <input
+          type="search"
+          class="search-field"
+          placeholder="Search for an anime..."
+          required
+          v-model='search_query'
+        />
+      </form>
+    </header>
+    <main>
+      <div class="cards">
+        <CardAnime/>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue';
+import CardAnime from "./components/CardAnime.vue"
 
 export default {
-  name: 'App',
+  setup(){
+      const search_query = ref("");
+    return {
+      search_query
+    }
+  },
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CardAnime,
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  font-family: "Fira sans", sans-serif;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  padding-top: 50px;
+  padding-bottom: 50px;
+
+  h1 {
+    color: #888;
+    font-size: 42px;
+    font-weight: 400;
+    text-align: center;
+    text-transform: uppercase;
+    margin-bottom: 30px;
+
+    strong {
+      color: #313131;
+    }
+  }
+
+  .search-box {
+    display: flex;
+    justify-content: center;
+    padding-left: 30px;
+    padding-right: 30px;
+
+    .search-field {
+      appearance: none;
+      background: none;
+      border: none;
+      outline: none;
+
+      background-color: #F3F3F3;
+
+      display: block;
+      width: 100%;
+      max-width: 600px;
+      padding: 15px;
+      border-radius:8px;
+
+      color: #313131;
+      font-size:20px;
+
+      transition: 0.4s;
+
+      &::placeholder{
+        color: #AAA;
+      }
+    }
+  }
+  main{
+    max-width: 1200px;
+    margin: 0 auto;
+    padding-left: 30px;
+    padding-right: 30px;
+
+    .cards{
+      display: flex;
+      flex-wrap: wrap;
+      margin: 0 -8px;
+    }
+  }
 }
 </style>
